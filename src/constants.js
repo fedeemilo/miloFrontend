75b2d606bfb22176add55a758fb1cda2942c0a05
Federico Milone <federico.milone@claro.com.ar>
2021-03-30 11:03:31 -0300

    update

--- src/constants.js
+++ src/constants.js
@@ -1,3 +1,3 @@
-export const __prod__ = process.env.NODE_END === 'production'
+export const __prod__ = process.env.NODE_ENV === 'production'
 
 export const __db__ = __prod__ ? 'https://milo-soft-backend.herokuapp.com' : 'http://localhost:8000'
